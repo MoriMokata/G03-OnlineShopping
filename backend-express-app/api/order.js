b9af17cc6ec1804da0c14426176a1cb956d4d588
f-wntp27 <wntp27th@gmail.com>
2022-11-11 14:01:25 +0700

    fix order create function

--- backend-express-app/api/order.js
+++ backend-express-app/api/order.js
@@ -5,54 +5,71 @@ const { cartSchema, productSchema } = require('../models/product');
 const { orderSchema } = require('../models/order');
 const authorization = require('../middlewares/authorize');
 
-const createOrder = (orderData) => {
-    return new Promise(async (resolve, reject) => {
-        const session = await mongoose.startSession();
-        session.startTransaction();
+const createOrder = async (orderData) => {
+    return new Promise((resolve, reject) => {
+        let Order = mongoose.model('orders', orderSchema);
+    
+        let checkStock = orderData.carts.map(item => checkProductStock(item))
+        Promise.all(checkStock)
+        .then(_ => {
+            let updateCart = orderData.carts.map(item => updateCarts(item))
+            Promise.all(updateCart)
+            .then(_ => {
+                Order.create(orderData, (err, data) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(data);
+                    }
+                })
+            })
+            .catch(err => { reject(err) });
+        })
+        .catch(err => { reject(err) });
+    });
+}
 
-        let Order = mongoose.model('orders', orderSchema)
-        Order.create(orderData, (err, data) => {
+const checkProductStock = (cartItem) => {
+    return new Promise((resolve, reject) => {
+        let Product = mongoose.model('products', productSchema);
+        Product.findById(cartItem.productId, (err, data) => {
             if (err) {
                 reject(err);
             } else {
-                let Cart = mongoose.model('carts', cartSchema);
-                data.carts.forEach(element => {
-                    Cart.findById(element._id, (cart_err, cart_data) => {
-                        if (cart_err) {
-                            session.abortTransaction();
-                            reject(cart_err);
-                        } else {
-                            Cart.findByIdAndUpdate(cart_data._id, element, (cart_update_err, cart_update_data) => {
-                                if (cart_update_err) {
-                                    session.abortTransaction();
-                                    reject(cart_update_err);
-                                }
-
-                                let Product = mongoose.model('products', productSchema);
-                                Product.findById(cart_data.productId, (product_err, product_data) => {
-                                    if (product_err) {
-                                        session.abortTransaction();
-                                        reject(product_err);
-                                    } else {
-                                        Product.findByIdAndUpdate(product_data._id, { quantity: (product_data.quantity - cart_update_data.quantity) }, (product_update_err) => {
-                                            if (product_update_err) {
-                                                session.abortTransaction();
-                                                reject(product_update_err);
-                                            }
-                                        });
-                                    }
-                                });
-                            });
-                        }
-                    });
-                });
-
-                session.endSession();
+                if (!data) {
+                    reject(new mongoose.Error('product not found'));
+                }
+                if (data.quantity - cartItem.quantity < 0) {
+                    reject(new mongoose.Error('product out of stock'));
+                }
                 resolve(data);
             }
         });
+    });
+}
 
-    });   
+const updateCarts = (cartItem) => {
+    return new Promise((resolve, reject) => {
+        let Product = mongoose.model('products', productSchema);
+        let Cart = mongoose.model('carts', cartSchema);
+
+        Cart.findByIdAndUpdate(cartItem._id, cartItem).
+        then(cartResult => {
+            Product.findById(cartResult.productId).then(product_result => {
+                Product.findByIdAndUpdate(product_result._id, { quantity: product_result.quantity - cartResult.quantity }, (err, data) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(cartResult);
+                    }
+                });
+            });
+        })
+        .catch(err => {
+            reject(err);
+        })
+        
+    });
 }
 
 const getOrderDetail = (id) => {
